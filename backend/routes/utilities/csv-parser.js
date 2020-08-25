a97014c11a00787aaadf5951a69d83536e1ff7c6
Larsen Friis <larsen.friis@uberflip.com>
2020-08-25 16:33:14 -0400

    CSV Parser function complete

--- backend/routes/utilities/csv-parser.js
+++ backend/routes/utilities/csv-parser.js
@@ -2,23 +2,23 @@ async function parseCSV (csv) {
 
     var tempResult = [];
     var tempObj = {};
-    var result = [];
-    
+    var result = [];    
     var rows = csv.split("\n");
 
+    // Removing carriage return character
     for (var i = 0; i < rows.length - 1; i++) {
         var newLine = rows[i].split("\r");
         tempObj = newLine[0];
         tempResult.push(tempObj)
     }
 
+    // Storing headers
     var headers = tempResult[0].split(",");
 
-    // Ignoring header, looping through temp result
+    // Building result JSON object
     for (var j = 1; j < tempResult.length; j++) {
         var currentRow = tempResult[j].split(",");
         var obj = {};
-
         for (var k = 0; k < headers.length; k++) {
                 obj[headers[k]] = currentRow[k];
         }
